e0aac2eb1cf12f5ec219653f9f71b9d419766adb
Adrian Gonzalez <adrian.gzz.escalante@gmail.com>
2022-09-02 16:07:46 -0500

    Se cambio de puerto

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const port = 8080;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || port);
 
 
 app.use(bodyParser.json({limit: '50mb'}));
