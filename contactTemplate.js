2583749abe85108ef9b03351e49a9a56c1e70310
Adrian Gonzalez <adrian.gzz.escalante@gmail.com>
2022-09-02 10:36:53 -0500

    initial commit

---
+++ contactTemplate.js
@@ -0,0 +1,23 @@
+var contactTemplate = ` <form action="/wp-admin/admin-ajax.php#wpcf7-f848-o1" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
+<div style="display: none;">
+<input type="hidden" name="_wpcf7" value="848">
+<input type="hidden" name="_wpcf7_version" value="5.6.3">
+<input type="hidden" name="_wpcf7_locale" value="en_US">
+<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f848-o1">
+<input type="hidden" name="_wpcf7_container_post" value="0">
+<input type="hidden" name="_wpcf7_posted_data_hash" value="">
+</div>
+<div class="wpcf7-response-output" aria-hidden="true"></div>
+<p><label>Nombre*</label><br>
+    <span class="wpcf7-form-control-wrap" data-name="Nombre"><input type="text" name="Nombre" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></p>
+<p>    <label>Correo*</label><br>
+    <span class="wpcf7-form-control-wrap" data-name="Correo"><input type="email" name="Correo" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span></p>
+<p>    <label>Teléfono</label><br>
+    <span class="wpcf7-form-control-wrap" data-name="Telefono"><input type="tel" name="Telefono" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-tel" aria-invalid="false"></span></p>
+<p>    <span class="wpcf7-form-control-wrap" data-name="Id"><input type="text" name="Id" value="" size="40" class="wpcf7-form-control wpcf7-text" id="idINMU" aria-invalid="false"></span><br>
+    <span class="wpcf7-form-control-wrap" data-name="Propiedad"><input type="text" name="Propiedad" value="" size="40" class="wpcf7-form-control wpcf7-text" id="propiedadINMU" aria-invalid="false"></span><br>
+    <span class="wpcf7-form-control-wrap" data-name="Direccion"><input type="text" name="Direccion" value="" size="40" class="wpcf7-form-control wpcf7-text" id="direccionINMU" aria-invalid="false"></span><br>
+    <span class="wpcf7-form-control-wrap" data-name="Ubicacion"><input type="text" name="Ubicacion" value="" size="40" class="wpcf7-form-control wpcf7-text" id="ubicacionINMU" aria-invalid="false"></span></p>
+<p>    <input type="submit" value="Submit" class="wpcf7-form-control has-spinner wpcf7-submit button"><span class="wpcf7-spinner"></span></p>
+<p style="display: none !important;"><label>Δ<textarea name="_wpcf7_ak_hp_textarea" cols="45" rows="8" maxlength="100"></textarea></label><input type="hidden" id="ak_js_1" name="_wpcf7_ak_js" value="1662073784835"></p></form>
+ `
